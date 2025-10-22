a69f4a11f43a8dbf8f783adf48f864f7aae57b9a
Ariel G <ariel.garces@uisek.edu.ec>
2025-10-22 12:03:44 -0500

    Finalización del laboratorio 3

---
+++ app/src/main/java/ec/edu/uisek/githubclient/Repoitem.kt
@@ -0,0 +1,49 @@
+package ec.edu.uisek.githubclient
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ec.edu.uisek.githubclient.databinding.FragmentRepoItemBinding
+
+class Repoitem : Fragment() {
+    private var _binding: FragmentRepoItemBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRepoItemBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.repoName.text = "Mi repositorio"
+        binding.repoDescription.text = "Este es el segundo texto"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            Repoitem().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
